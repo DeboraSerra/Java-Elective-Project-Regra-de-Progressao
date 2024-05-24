4dd36bc0509d5914500ec5af0602325b835aa8e6
Débora Serra <debora.r.serra@gmail.com>
2024-05-24 12:39:46 -0300

    feat: add requirement one

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,19 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int numOfProjects = Integer.parseInt(scanner.nextLine());
+    int[] gradeWheights = new int[numOfProjects];
+    String[] names = new String[numOfProjects];
+    for (int n = 0; n < numOfProjects; n += 1) {
+      System.out.println("Digite o nome da atividade " + (n + 1) + ":");
+      names[n ] = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + (n + 1) + ":");
+      gradeWheights[n] =Integer.parseInt(scanner.nextLine()) ;
+    }
+    scanner.close();
+    System.out.println(gradeWheights);
+    System.out.println(names);
   }
 }
